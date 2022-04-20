675c260203f441b1841651336c2e40a5749ba93e
K-Sato <katsuki_sato0130@icloud.com>
2022-04-20 13:22:39 +0900

    Layouts(レイアウト)

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<div class="container">
+	<!-- 全てのページでヘッダーを表示する -->
+	<nav class="header">
+		<h1 class="title">SvelteKit Demo</h1>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+	</nav>
+
+	<!-- ページ内容をここに入れ込む -->
+	<slot />
+
+	<!-- 全てのページでfooterを表示する -->
+	<footer class="footer">
+		<h1 class="title">SvelteKit Demo</h1>
+	</footer>
+</div>
--- src/routes/index.svelte
+++
@@ -1,2 +0,0 @@
-<a href="/about">About</a>
-<a href="/posts/examle1">詳細ページ</a>
