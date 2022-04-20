ad284be165d76ffd04758886831d4b40d5117c72
K-Sato <katsuki_sato0130@icloud.com>
2022-04-20 16:52:13 +0900

    Load関数

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,21 @@
-<h1>Home</h1>
+<script context="module">
+	export async function load({ fetch }) {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+		return {
+			props: {
+				posts: response.ok && (await response.json())
+			}
+		};
+	}
+</script>
+
+<script>
+	export let posts;
+</script>
+
+<ul class="post-list">
+	{#each posts as post}
+		<li><a href={`posts/${post.id}`} class="post-link">{post.title}</a></li>
+	{/each}
+</ul>
+
--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,1 +1,21 @@
-<h1>詳細ページ</h1>
+<script context="module">
+	export async function load({ fetch, params }) {
+		const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.slug}`);
+		return {
+			props: {
+				post: response.ok && (await response.json())
+			}
+		};
+	}
+</script>
+
+<script>
+	export let post;
+</script>
+
+<div class="post">
+	<h2>{post.title}</h2>
+	<p>{post.body}</p>
+</div>
+
+
