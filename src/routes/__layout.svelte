6023df822b13cc42cb1d45a3bfb14f2933e25729
K-Sato <katsuki_sato0130@icloud.com>
2022-04-20 13:30:31 +0900

    Layouts デザイン

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -14,3 +14,48 @@
 		<h1 class="title">SvelteKit Demo</h1>
 	</footer>
 </div>
+
+<style>
+	/* Fontの設定 */
+	:global(body) {
+		font-family: Helvetica, Arial, sans-serif;
+	}
+
+	.header {
+		width: 860px;
+		background-color: #ff6600;
+		display: flex;
+		padding: 5px 0;
+	}
+
+	.header > a {
+		text-decoration: none;
+		color: #000;
+		margin-right: 10px;
+	}
+
+	.header > a:not(:last-child)::after {
+		content: '|';
+		margin-left: 10px;
+	}
+
+	.title {
+		font-size: 1rem;
+		margin: 0;
+		margin-right: 10px;
+	}
+
+	.container {
+		width: 860px;
+		margin: 0 auto;
+		background-color: #f6f6ef;
+	}
+
+	.footer {
+		border-top: solid 2px #ff6600;
+		height: 100px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
